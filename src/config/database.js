48a5f4e27dbc7c88decb2c531d3c9df6fb9b0c70
Glauber <glaubersantiagodecastro@gmail.com>
2023-09-17 22:05:45 -0300

    CRIACAO DE BD

--- src/config/database.js
+++ src/config/database.js
@@ -13,11 +13,29 @@
 //     }
 // }
 
+
+//ORIGINAL
+// module.exports = {
+//     dialect: 'postgres',
+//     host: 'localhost',
+//     username: 'postgres',
+//     password: '1234',
+//     database: 'codeburguer',
+//     define: {
+//         timestamps: true,
+//         underscored: true,
+//         underscoredAll: true
+//     }
+    
+// }
+
+
+
 module.exports = {
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: '1234',
+    dialect: 'codeburguer',
+    host: 'codeburguer.ckymwbkfxdvf.sa-east-1.rds.amazonaws.com',
+    username: 'codeburguer',
+    password: '12341234',
     database: 'codeburguer',
     define: {
         timestamps: true,
