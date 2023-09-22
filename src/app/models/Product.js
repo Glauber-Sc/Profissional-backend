19fb305fc3cbcc85eff30cb4a7c5b1df7f686d57
Glauber <glaubersantiagodecastro@gmail.com>
2023-09-22 01:27:21 -0300

    ATUALIZACAO DE IP httpS

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -12,7 +12,7 @@ class Product extends Model {
                 url: {
                     type: Sequelize.VIRTUAL,
                     get() {
-                        return `https://177.71.203.249:3000/product-file/${this.path}`
+                        return `https://pedepede.fun:3000/product-file/${this.path}`
                     }
                 }
             },
