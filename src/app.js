07ea9c18c25b86263773f0c3a7e8f38c61eec24a
Glauber <glaubersantiagodecastro@gmail.com>
2023-10-07 12:16:25 -0300

    ATUALIZACAO 1

--- src/app.js
+++ src/app.js
@@ -8,7 +8,13 @@ class App {
     constructor() {
         this.app = express()
 
-        this.app.use(cors())
+        const corsOptions = {
+            origin: 'https://pedepede.fun', // Especifique o domínio permitido
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+            credentials: true, // Permite credenciais (cookies, cabeçalhos de autenticação)
+          };
+
+        this.app.use(cors(corsOptions))
 
         this.middlewares()
         this.routes()
