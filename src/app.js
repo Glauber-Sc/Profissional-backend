eb8ee0bf43c00b929d37f2228f75d0651fc8591b
Glauber <glaubersantiagodecastro@gmail.com>
2023-10-07 13:17:10 -0300

    ATUALIZACAO 3

--- src/app.js
+++ src/app.js
@@ -1,41 +1,44 @@
-import express from 'express'
-import routes from './routes'
-import { resolve } from 'path'
-import cors from 'cors'
+import express from "express";
+import routes from "./routes";
+import { resolve } from "path";
+import cors from "cors";
 
-import './database'
+import "./database";
 class App {
     constructor() {
-        this.app = express()
+        this.app = express();
 
-        const corsOptions = {
-            origin: 'https://pedepede.fun', // Especifique o domínio permitido
-            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-            credentials: true, // Permite credenciais (cookies, cabeçalhos de autenticação)
-          };
+        // Configuração do CORS para permitir solicitações do domínio do frontend
+        app.use(
+            cors({
+                origin: "https://pedepede.fun", // Substitua pelo domínio correto do seu frontend
+                methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+                credentials: true, // Permite credenciais (cookies, cabeçalhos de autenticação)
+            })
+        );
 
-        this.app.use(cors(corsOptions))
+       // this.app.use(cors(corsOptions));
 
-        this.middlewares()
-        this.routes()
+        this.middlewares();
+        this.routes();
     }
 
     middlewares() {
-        this.app.use(express.json())
+        this.app.use(express.json());
         this.app.use(
-            '/product-file', // Avisa qual rota vai servir os arquivos estaticos
-            express.static(resolve(__dirname, '..', 'uploads')) // Procura o arquivo correspondente ao nome que geramos
-        )
+            "/product-file", // Avisa qual rota vai servir os arquivos estaticos
+            express.static(resolve(__dirname, "..", "uploads")) // Procura o arquivo correspondente ao nome que geramos
+        );
 
         this.app.use(
-            '/category-file', // Avisa qual rota vai servir os arquivos estaticos
-            express.static(resolve(__dirname, '..', 'uploads')) // Procura o arquivo correspondente ao nome que geramos
-        )
+            "/category-file", // Avisa qual rota vai servir os arquivos estaticos
+            express.static(resolve(__dirname, "..", "uploads")) // Procura o arquivo correspondente ao nome que geramos
+        );
     }
 
     routes() {
-        this.app.use(routes)
+        this.app.use(routes);
     }
 }
 
-export default new App().app
+export default new App().app;
