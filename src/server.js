eb363ed9bef141df0b568bdf492d592efd996ca1
Glauber <glaubersantiagodecastro@gmail.com>
2023-10-07 22:39:06 -0300

    URL 13

--- src/server.js
+++ src/server.js
@@ -8,11 +8,6 @@ import app from './app'
 // app.listen(3001)
 
 
-// app.listen(3000, () => {
-//     console.log("running...");
-//   });
-
-
-server.listen(3000, '0.0.0.0', () => {
-  console.log('Servidor Node.js está ouvindo na porta 3000');
-});
+app.listen(3000, () => {
+    console.log("running...");
+  });
