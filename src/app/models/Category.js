e60554a8f555a7f1b70a4d212470503626d7e84e
Glauber <glaubersantiagodecastro@gmail.com>
2023-10-11 17:37:03 -0300

    imagem 2

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -12,9 +12,13 @@ class Category extends Model {
                     // Este campo não existe no banco de dados
                     // Gerando uma url quando o usuario solicita informações do produto
                     type: Sequelize.VIRTUAL,
+                    // get() {
+                    //     return `https://localhost:3000/category-file/${this.path}`
+                    // }
                     get() {
-                        return `https://localhost:3000/category-file/${this.path}`
+                        return `/category-file/${this.path}`;
                     }
+                    
                 }
             },
             {
